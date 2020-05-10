36de025fcb4c2b942cf9cdf09eee66ed0d2183c4
uka151 <umeshagrahari87@gmail.com>
2020-05-10 12:17:41 +0530

    ingredient update

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import classes from '../Burger/Burger.module.css';
-import BurgerIncredient from '../Burger/BurgerIncredient/BurgerIncredient.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger =(props) =>{
 
+// Object.key is javascript method that extract key & return an array which contain string of key in end
+const tranfromedIngredients =Object.keys(props.ingredients)
+.map(igKey=>{
+    return [...Array(props.ingredients[igKey])].map((_, i)=>{
+       return <BurgerIngredient key={igKey+i} type={igKey}/>;
+    })
+})
+.reduce((arr, el)=>{
+    return arr.concat(el)
+},[]);
+
+console.log(tranfromedIngredients);
+
+
     return(
         <div className={classes.Burger}>
-            <BurgerIncredient type="bread-top"/>
-            <BurgerIncredient type="cheese"/>
-            <BurgerIncredient type="salad"/>
-            <BurgerIncredient type="bread-bottom"/>
+            
+           <BurgerIngredient type="bread-top"/>
+                {tranfromedIngredients}
+           <BurgerIngredient type="bread-bottom"/>
+       
         </div>
     )
-}
+    }
 
 export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,11 +4,21 @@ import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component{
 
+    state ={
+        ingredients:{
+            salad: 1,
+            bacon: 1,
+            cheese: 0,
+            meat: 1,
+        }
+    }
+
+
 
     render(){
         return(
             <Aux>
-              <Burger/>
+              <Burger ingredients={this.state.ingredients}/>
                 <div>Burger Control</div>
             </Aux>
 
